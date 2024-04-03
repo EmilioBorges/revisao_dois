2e56274cd38d6b1d964ba6cf763eabbabfb880dc
EmilioBorges <emilio.borges2012@gmail.com>
2024-04-02 21:03:09 -0300

    resolve questao 7

--- condicionaios.py
+++ condicionaios.py
@@ -1,14 +1,26 @@
-def verifica_maior_idade():
-    idade = int(input('Digite sua idade: '))
+# def verifica_maior_idade():
+#     idade = int(input('Digite sua idade: '))
 
-    if idade <= 0: {
-        print('Digite um numero maior que Zero')
+#     if idade <= 0: {
+#         print('Digite um numero maior que Zero')
 
-    }
-    elif idade >= 18:
-        print(f'Você tem {idade} anos então e de maior')
-    else:
-        print(f'Você tem {idade} anos então e de menor')
+#     }
+#     elif idade >= 18:
+#         print(f'Você tem {idade} anos então e de maior')
+#     else:
+#         print(f'Você tem {idade} anos então e de menor')
               
     
-verifica_maior_idade()
+# verifica_maior_idade()
+
+
+def par_impar():
+    numero = int(input('Digite um numero: '))
+
+    if numero % 2 == 0:
+        print('Numero par')
+    else:
+        print('Numero impar')
+
+
+par_impar()
